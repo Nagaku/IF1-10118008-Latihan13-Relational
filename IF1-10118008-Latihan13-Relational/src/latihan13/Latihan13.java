d74abacc99b078727a9e2aa2f0e6f431f9aeaeaa
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:42:52 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan13-Relational/src/latihan13/Latihan13.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan13;
+
+/**
+ *
+ *  @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan13 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		
+		int bil1 = 10;
+		int bil2 = 20;
+		System.out.println("a == b = " + (bil1 == bil2));
+		System.out.println("a != b = " + (bil1 != bil2));
+		System.out.println("a > b = " + (bil1 > bil2));
+		System.out.println("a < b = " + (bil1 < bil2));
+		System.out.println("b >= a = " + (bil2 >= bil1));
+		System.out.println("b <= a = " + (bil2 <=bil1));
+	}	
+	
+}
